10323659b93aba5d65666ac21ada5756dce9f7fd
Leonardo <serafa.leo@proton.me>
2025-03-08 15:19:55 -0300

    Implemented unit tests for Refresh method in UsersService.

--- Wingman.Tests/Unit/UsersServiceTests.cs
+++ Wingman.Tests/Unit/UsersServiceTests.cs
@@ -95,8 +95,8 @@ public class UsersServiceTests
 
         user.PasswordHash = new PasswordHasher<User>().HashPassword(user, loginDto.Password);
 
-        const string accessToken = "AccessToken";
-        const string refreshToken = "RefreshToken";
+        const string accessToken = "Some Access Token";
+        const string refreshToken = "Some Refresh Token";
 
         _mockRepo.Setup(repo => repo.GetUserByEmailAsync(It.IsAny<string>())).ReturnsAsync(user);
         _mockTokenService.Setup(ts => ts.GenerateAccessToken(user)).Returns(accessToken);
@@ -167,6 +167,168 @@ public class UsersServiceTests
         Assert.False(result.Success);
         Assert.Equal("Email or password wrong.", result.Message);
         Assert.Null(result.Data);
+        Assert.Null(result.Errors);
+    }
+
+    #endregion
+
+    #region Refresh
+    [Fact]
+    public async Task Refresh_ShouldReturnNotFound_WhenUserDoesNotExists()
+    {
+        // Arrange
+        RefreshRequestDto refreshDto = new()
+        {
+            RefreshToken = "Some Refresh Token",
+            UserId = Guid.NewGuid()
+        };
+
+        _mockRepo.Setup(repo => repo.GetByIdAsync(refreshDto.UserId)).ReturnsAsync((User?)null);
+
+        // Act
+        ApiResponseDto<TokenResponseDto> result = await _service.Refresh(refreshDto);
+
+        // Assert
+        Assert.Equal(StatusCodes.Status404NotFound, result.StatusCode);
+        Assert.False(result.Success);
+        Assert.Equal("User not found.", result.Message);
+        Assert.Null(result.Data);
+        Assert.Null(result.Errors);
+    }
+
+    [Fact]
+    public async Task Refresh_ShouldReturnBadRequest_WhenRefreshTokenInDatabaseIsNull()
+    {
+        // Arrange
+        RefreshRequestDto refreshDto = new()
+        {
+            RefreshToken = "Some Refresh Token",
+            UserId = Guid.NewGuid()
+        };
+
+        User user = new()
+        {
+            Id = refreshDto.UserId,
+            Email = "test@test.com",
+            PasswordHash = "Some Hash",
+            RefreshToken = null,
+            RefreshTokenExpirationDateTimeUTC = null
+        };
+
+        _mockRepo.Setup(repo => repo.GetByIdAsync(refreshDto.UserId)).ReturnsAsync(user);
+
+        // Act
+        ApiResponseDto<TokenResponseDto> result = await _service.Refresh(refreshDto);
+
+        // Assert
+        Assert.Equal(StatusCodes.Status400BadRequest, result.StatusCode);
+        Assert.False(result.Success);
+        Assert.Equal("Invalid Refresh Token.", result.Message);
+        Assert.Null(result.Data);
+        Assert.Null(result.Errors);
+    }
+
+    [Fact]
+    public async Task Refresh_ShouldReturnBadRequest_WhenRefreshTokenIsDifferentFromDatabase()
+    {
+        // Arrange
+        RefreshRequestDto refreshDto = new()
+        {
+            RefreshToken = "Wrong Refresh Token",
+            UserId = Guid.NewGuid()
+        };
+
+        User user = new()
+        {
+            Id = refreshDto.UserId,
+            Email = "test@test.com",
+            PasswordHash = "Some Hash",
+            RefreshToken = "Right Refresh Token",
+            RefreshTokenExpirationDateTimeUTC = DateTime.UtcNow.AddDays(7)
+        };
+
+        _mockRepo.Setup(repo => repo.GetByIdAsync(refreshDto.UserId)).ReturnsAsync(user);
+
+        // Act
+        ApiResponseDto<TokenResponseDto> result = await _service.Refresh(refreshDto);
+
+        // Assert
+        Assert.Equal(StatusCodes.Status400BadRequest, result.StatusCode);
+        Assert.False(result.Success);
+        Assert.Equal("Invalid Refresh Token.", result.Message);
+        Assert.Null(result.Data);
+        Assert.Null(result.Errors);
+    }
+
+    [Fact]
+    public async Task Refresh_ShouldReturnUnauthorized_WhenRefreshTokenIsExpired()
+    {
+        // Arrange
+        RefreshRequestDto refreshDto = new()
+        {
+            RefreshToken = "Some Refresh Token",
+            UserId = Guid.NewGuid()
+        };
+
+        User user = new()
+        {
+            Id = refreshDto.UserId,
+            Email = "test@test.com",
+            PasswordHash = "Some Hash",
+            RefreshToken = refreshDto.RefreshToken,
+            RefreshTokenExpirationDateTimeUTC = DateTime.UtcNow.AddDays(-1)
+        };
+
+        _mockRepo.Setup(repo => repo.GetByIdAsync(refreshDto.UserId)).ReturnsAsync(user);
+
+        // Act
+        ApiResponseDto<TokenResponseDto> result = await _service.Refresh(refreshDto);
+
+        // Assert
+        Assert.Equal(StatusCodes.Status401Unauthorized, result.StatusCode);
+        Assert.False(result.Success);
+        Assert.Equal("Refresh Token is expired. A new login is necessary.", result.Message);
+        Assert.Null(result.Data);
+        Assert.Null(result.Errors);
+    }
+
+    [Fact]
+    public async Task Refresh_ShouldReturnOkAndNewToken_WhenSuccessful()
+    {
+        // Arrange
+        RefreshRequestDto refreshDto = new()
+        {
+            RefreshToken = "Some Refresh Token",
+            UserId = Guid.NewGuid()
+        };
+
+        User user = new()
+        {
+            Id = refreshDto.UserId,
+            Email = "test@test.com",
+            PasswordHash = "Some Hash",
+            RefreshToken = refreshDto.RefreshToken,
+            RefreshTokenExpirationDateTimeUTC = DateTime.UtcNow.AddDays(1)
+        };
+
+        const string accessToken = "New Access Token";
+        const string refreshToken = "New Refresh Token";
+
+        _mockRepo.Setup(repo => repo.GetByIdAsync(refreshDto.UserId)).ReturnsAsync(user);
+        _mockTokenService.Setup(ts => ts.GenerateAccessToken(user)).Returns(accessToken);
+        _mockTokenService.Setup(ts => ts.GenerateRefreshToken()).Returns(new RefreshToken() { Token = refreshToken, Expiration = DateTime.UtcNow.AddDays(7) });
+
+        // Act
+        ApiResponseDto<TokenResponseDto> result = await _service.Refresh(refreshDto);
+
+        // Assert
+        Assert.Equal(StatusCodes.Status200OK, result.StatusCode);
+        Assert.True(result.Success);
+        Assert.Equal("Refresh successful.", result.Message);
+        Assert.NotNull(result.Data);
+        Assert.Equal(user.Id, result.Data.UserId);
+        Assert.Equal(refreshToken, result.Data.RefreshToken);
+        Assert.Equal(accessToken, result.Data.AccessToken);
         Assert.Null(result.Errors);
     }
 
